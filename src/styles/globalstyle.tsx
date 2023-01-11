42c72fdb2863ebfdebd9bc45625264a06f442c18
Walyson Moura <walysonmoura222@gmail.com>
2023-01-11 16:44:02 -0300

    new

--- src/styles/globalstyle.tsx
+++ src/styles/globalstyle.tsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   html,
   body {
-    color: ${({ theme }) => theme.colors.primary};
+   
     padding: 0;
  
     margin: 0;
